19f9000edb9224641f32f4528a4e32b48e49c51d
Angel Jesus Estrada <AEstrada@transborder.com.co>
2023-11-20 12:29:54 -0500

    - Send Grid . se creo microservicio para recibir de una cola dee activeMQ . se consume otro microservicio que envia la plantilla

---
+++ mc-receive-sendgrid/mc-receive-sendgrid/src/main/java/com/sendgrid/mcreceivesendgrid/McReceiveSendgridApplication.java
@@ -0,0 +1,13 @@
+package com.sendgrid.mcreceivesendgrid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McReceiveSendgridApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McReceiveSendgridApplication.class, args);
+	}
+
+}
---
+++ mc-receive-sendgrid/mc-receive-sendgrid/src/main/java/com/sendgrid/mcreceivesendgrid/dto/ReceivePlantilla.java
@@ -0,0 +1,14 @@
+package com.sendgrid.mcreceivesendgrid.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ReceivePlantilla {
+    private int id;
+    private String nombre;
+    private String content;
+}
